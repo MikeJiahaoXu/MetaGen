17f48dd395a18ae8f5b7124659eeb0c503f2db13
MikeJiahaoXu <mikeke.xu@mail.utoronto.ca>
2025-12-02 16:47:35 +0800

    small modification

--- generation/main.py
+++ generation/main.py
@@ -8,7 +8,7 @@ import argparse
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--config', default="config.json", help='path to config.json', type=str, default="config.json")
+    parser.add_argument('--config', help='path to config.json', type=str, default="config.json")
     args = parser.parse_args()
     
     with open(args.config, 'r', encoding='utf-8') as file:
